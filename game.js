ab80cf5d60c3eefe19b52f64503848af11eebd57
Charles Merriam <charles.merriam@gmail.com>
2013-01-03 18:05:53 -0800

    Add collision for blue block hits red block.
    When the blue block hits the red block, the collision is detected and
    the blue block is returned to its starting point.  The empty component
    name "Blueable" is added to the red block.  The blue block uses the
    Crafty Collision component and references Blueable in the onHit() check.
    This is the final basic game from which all other games are mere
    enhancements.

--- game.js
+++ game.js
@@ -4,11 +4,15 @@ window.onload = function() {
 	Crafty.background("yellow");     
 	// Crafty.canvas.init();
 	
-	var eRed = Crafty.e("2D, DOM, Color")
+	var eRed = Crafty.e("2D, DOM, Color, Blueable")
 				.color("red")
 				.attr({w: 50, h: 50, x:170, y:200}),
-		eBlue = Crafty.e("2D, DOM, Multiway, Color")
+		eBlue = Crafty.e("2D, DOM, Multiway, Color, Collision")
 				.multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
-				.color("blue").attr({w: 60, h: 60, x:70, y:200});
+				.color("blue")
+				.attr({w: 60, h: 60, x:70, y:200})
+				.onHit("Blueable", 
+					function () { this.attr({x: 70, y:200}); }
+				);
 	
 };

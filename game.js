8e0c58560dd7ef763747d55219f5f7db945227e3
Charles Merriam <charles.merriam@gmail.com>
2013-01-03 17:37:17 -0800

    Add red and blue block entities with components for display and movement.
    The play area now has a yellow background.   Then I added two
    "entities":  red block and blue block.  The components add sets of
    behaviors to each block.  The red block has these components: 2D to make
    it a 2D object on the canvas and to give it the x, y, h (height), and w
    (width) attributes; DOM to animate using a DIV in the DOM; and Color to
    give it a color and the color() method.   The blue block also has
    multiway to give the block movement in four directions including mashing
    keys for diagonals.   Also added a yellow background to the game area.

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 window.onload = function() {
 	//start crafty
 	Crafty.init(400, 320);
+	Crafty.background("yellow");     
 	// Crafty.canvas.init();
-	foobar;
+	
+	var eRed = Crafty.e("2D, DOM, Color")
+				.color("red")
+				.attr({w: 50, h: 50, x:170, y:200}),
+		eBlue = Crafty.e("2D, DOM, Multiway, Color")
+				.multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
+				.color("blue").attr({w: 60, h: 60, x:70, y:200});
+	
 };
